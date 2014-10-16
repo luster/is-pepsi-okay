ba22ddb7c6ebef0a140d0ba0e475cab9b868ddeb
luster <ethan.lusterman@gmail.com>
2014-10-16 19:15:46 -0400

    added some mysql stuff

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask.ext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ispepsiokay'
+app.config['MYSQL_DATABASE_DB'] = 'IsPepsiOkay'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
+    cursor = mysql.get_db().cursor()
     return "It's working, yayyy"
 
 if __name__ == '__main__':
