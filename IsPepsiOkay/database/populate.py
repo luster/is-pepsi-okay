132e067a8b299e339dc9f2ecfcb16e9e30a49c9d
luster <ethan.lusterman@gmail.com>
2014-11-13 23:27:14 -0500

    some web parse

--- IsPepsiOkay/database/populate.py
+++ IsPepsiOkay/database/populate.py
@@ -3,6 +3,7 @@
 import os
 import MySQLdb
 import sys
+from bs4 import BeautifulSoup
 
 PROJECT_DIR = os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -22,4 +23,16 @@ db = MySQLdb.connect(
 
 cur = db.cursor()
 
+# Code to generate Actor File Structure
+with open(DATA_DIR + '/uci/actors.html.better', 'r') as f:
+    num = 0
+    soup = BeautifulSoup(f.read())
+    tbl = soup.findAll('table')
+    for table in tbl:
+        for row in table.findAll('tr')[1:]:
+            if len(row.findAll('td')) > 0:
+                num += 1
+                print row.findAll('td')[0].contents[0][1:]
+
 print 'Works'
+print num
