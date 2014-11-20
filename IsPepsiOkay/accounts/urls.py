077dd1a4ebf8b9930cb1635c11ca8a4c77cb9692
JoeyMercs <joeymercs@gmail.com>
2014-11-20 16:26:48 -0500

    Some user backend

---
+++ IsPepsiOkay/accounts/urls.py
@@ -0,0 +1,11 @@
+urlpatterns = patterns('accounts.views',
+url(r'^register/$', 'register'),
+url(r'^delete/$', 'delete'),
+url(r'^login/$', login),
+url(r'^logout/$', logout),
+url(r'^password/change/$', password_change),
+url(r'^password/change/success/$', password_change_done),
+url(r'^password/reset/$', password_reset),
+url(r'^password/reset/success/$', password_reset_done),
+url(r'^email/change/$', 'change_email'),
+)
--- IsPepsiOkay/models.py
+++ IsPepsiOkay/models.py
@@ -1,6 +1,18 @@
+from flask.ext.login import UserMixin
 
-class User():
-    pass
+class User(UserMixin):
+    
+    def __init__(self, username, email, password, active=True):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.active = active
+        
+    def is_active(self):
+        return self.active
+        
+    def get_id(self)
+        return str(self.username)
 
 class Movie():
     pass
